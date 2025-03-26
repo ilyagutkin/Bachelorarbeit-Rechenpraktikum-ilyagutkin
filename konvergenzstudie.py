9b64cf98a40da847368663414db37793dd27a5b7
Ilya Gutkin <ilya.gutkin@stud.uni-goettingen.de>
2025-03-26 19:02:55 +0100

    verbesserung der Visualisierung von den L2 Fehlern der KOnvergenzmethoden

--- konvergenzstudie.py
+++ konvergenzstudie.py
@@ -3,6 +3,9 @@ from ngsolve import *
 from netgen.geom2d import SplineGeometry
 from ngsolve.webgui import Draw
 from netgen.occ import *
+from tabulate import tabulate
+import pandas as pd
+import numpy as np
 
 def CG_solver(mesh,order,w,eps):
     fes = H1(mesh, order=order, dirichlet="bottom|right|left")
@@ -103,12 +106,16 @@ def SUPG_solver(mesh,order,w,eps):
     gfu.vec.data += a.mat.Inverse(fes.FreeDofs()) * res
     return(gfu)
 
+def addtuple(t1,t2):
+        return tuple(a + b for a, b in zip(t1, t2))
+
 def maxy(mesh,deformation):
         max_y_difference = 0  # Variable zur Speicherung der größten y-Differenz
         max_x_difference = 0  # Variable zur Speicherung der größten x-Differenz
         # Iteriere über alle Dreiecke im Mesh
         for el in mesh.Elements():
-                vertices = [tuple(deformation(mesh[vi].point[0], mesh[vi].point[1])) for vi in el.vertices]  # Eckpunkte des Dreiecks
+                
+                vertices = [addtuple(tuple(deformation(mesh[vi].point[0],mesh[vi].point[1])),tuple(mesh[vi].point)) for vi in el.vertices]  # Eckpunkte des Dreiecks
                 y_coords = [p[1] for p in vertices]  # Nur die y-Koordinaten extrahieren
                 x_coords = [p[0] for p in vertices]  # Nur die x-Koordinaten extrahieren
 
@@ -119,31 +126,54 @@ def maxy(mesh,deformation):
                 max_x_difference = max(max_x_difference, x_difference)  # Maximale Differenz speichern
         return max_y_difference, max_x_difference
 
-Meshes =[]
-for p in range(-3,1):
-        for i in range(-3,1):
-                L = 2**i
-                T = 2**(p) # length of the time interval
-                shape = Rectangle(L,T).Face()
-                shape.edges.Min(X).name="left"
-                shape.edges.Max(X).name="right"
-                shape.edges.Min(Y).name="bottom"
-                shape.edges.Max(Y).name="top"
-                mesh = Mesh(OCCGeometry(shape, dim=2).GenerateMesh(maxh=0.05))
-                deformation = GridFunction(H1(mesh,order=1,dim=mesh.dim))
-                deformation.Set(CF((2**-i*x,2**(-p)*y))-CF((x,y)))
-                mesh.SetDeformation(deformation)
-                maximal_y_difference, maximal_x_difference = maxy(mesh,deformation)
-                Meshes.append((mesh,maximal_y_difference,maximal_x_difference))
+def generate_mesh(x_steps=5,y_steps=5):
+    Meshes =[]
+    for p in range(-3,-3+y_steps):
+            mesh1=[]
+            for i in range(-3,-3+x_steps):
+                    L = 2**i
+                    T = 2**(p) # length of the time interval
+                    shape = Rectangle(L,T).Face()
+                    shape.edges.Min(X).name="left"
+                    shape.edges.Max(X).name="right"
+                    shape.edges.Min(Y).name="bottom"
+                    shape.edges.Max(Y).name="top"
+                    mesh = Mesh(OCCGeometry(shape, dim=2).GenerateMesh(maxh=0.025))
+                    deformation = GridFunction(H1(mesh,order=1,dim=mesh.dim))
+                    deformation.Set(CF((2**-i*x,2**(-p)*y))-CF((x,y)))
+                    mesh.SetDeformation(deformation)
+                    maximal_y_difference, maximal_x_difference = maxy(mesh,deformation)
+                    mesh1.append((mesh,maximal_y_difference,maximal_x_difference))
+            Meshes.append(mesh1)
+    return Meshes
 
 Solver =[CG_solver, DG_solver, SUPG_solver]
 w = 1
 eps =1
+Meshes = generate_mesh(5,5)
+
+df1 = pd.DataFrame(Meshes)
+# Extrahiere die ersten und zweiten Werte der Tupel in separate Listen
+first_values = [[item[1] for item in row] for row in df1.values]
+second_values = [[item[2] for item in row] for row in df1.values]
+deltat = [np.mean(row) for row in first_values] 
+deltax= [np.mean(col) for col in zip(*second_values)] 
 
 for order in range(3):
     for solver in Solver:
-        for mesh in Meshes:
-            gfu = solver(mesh[0],order,w,eps)
-            L2fehler = sqrt(Integrate((gfu-(sin(pi*x)*exp(-(pi**2+w**2/4)*y)*exp(w*x/2)))**2, mesh[0]))
-            print(f"Methode: {solver.__name__} , order ={order} L2 Fehler: {L2fehler} max_y ={mesh[1]} max_x={mesh[2]}" )
+        L2fehler = zeros((len(Meshes),len(Meshes[0])))
+        for i in range(len(Meshes)):
+            for j in range(len(Meshes[i])):
+                mesh = Meshes[i][j]
+                gfu = solver(mesh[0],order,w,eps)
+                L2fehler[i][j] = sqrt(Integrate((gfu-(sin(pi*x)*exp(-(pi**2+w**2/4)*y)*exp(w*x/2)))**2, mesh[0]))
+                #print(f"Methode: {solver.__name__} , order ={order} L2 Fehler: {L2fehler} max_y ={mesh[1]} max_x={mesh[2]}" )
+        #print(tabulate(L2fehler, tablefmt="grid"))  
+        df = pd.DataFrame(L2fehler, columns = deltax, index = deltat)
+        df.index.name = "Δt / Δx"
+        df.columns.name = " "
+        table_name = f"{solver.__name__}_order_:{order}"
+        print(f"Table: {table_name}")
+        print(df) 
+        print("\n")
         
