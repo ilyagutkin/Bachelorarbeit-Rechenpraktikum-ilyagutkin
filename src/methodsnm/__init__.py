6f03990f7e8365b15bcdf44919c2d5d4c2f787b2
Ilya Gutkin <ilya.gutkin@stud.uni-goettingen.de>
2025-03-31 14:32:52 +0200

    erweiterung der initdatei

--- src/methodsnm/__init__.py
+++ src/methodsnm/__init__.py
@@ -3,12 +3,14 @@ print("source module for methodsNM imported.")
 import methodsnm.fe
 import methodsnm.fe_1d
 import methodsnm.fe_2d
+import methodsnm.fe_4d
 import methodsnm.fes
 import methodsnm.formint
 import methodsnm.forms
 import methodsnm.intrule
 import methodsnm.intrule_1d
 import methodsnm.intrule_2d
+import methodsnm.intrule_4d
 import methodsnm.mesh
 import methodsnm.meshfct
 import methodsnm.recpol
